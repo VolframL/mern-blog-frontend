17fa045112f0510f98e9962123ffa541b54473b2
Vitalii <volfram@ukr.net>
2023-02-22 00:16:11 +0200

    sleep

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
+
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+
+  const onClickLogout = () => {
+    if (window.confirm('Вы действительно хотите выйти?'));
+    dispatch(logout());
+    window.localStorage.removeItem('token');
+  };
 
   return (
     <div className={styles.root}>
